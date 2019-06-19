e26cc802de1a9b8566e59d5cb12b501d29f098d5
Alvin <alvinchun911@gmail.com>
2019-06-18 21:43:26 -0400

    updated

--- src/components/Product.js
+++ src/components/Product.js
@@ -13,17 +13,49 @@ export default class Product extends Component {
                     inCart
                   } = this.props.product
                     return (
-                        <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+                      <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
                         <div className="card">
-                        <div key={id} className="img-container p-5" onClick={console.log("you clicked on the image container")}>
-                                <Link to="/details">
-                                        <img key={id} src={img} alt="product" className="card-img-top" />
-                                </Link>
-                                <button className="cart-btn" disabled={inCart? true:false}></button>
+                          <div
+                            key={id}
+                            className="img-container p-5"
+                            onClick={console.log(
+                              "you clicked on the image container"
+                            )}
+                          >
+                            <Link to="/details">
+                              <img
+                                key={id}
+                                src={img}
+                                alt="product"
+                                className="card-img-top"
+                              />
+                            </Link>
+                            <button
+                              className="cart-btn"
+                              disabled={
+                                inCart ? true : false
+                              }
+                              onClick={() => {
+                                console.log(
+                                  "added to the cart"
+                                );
+                              }}
+                            >
+                              {inCart ? (
+                                <p
+                                  className="text-capitalize mb-0"
+                                  disabled
+                                >
+                                  in inCart
+                                </p>
+                              ) : (
+                                <i className="fas fa-cart plus" />
+                              )}
+                            </button>
+                          </div>
                         </div>
-                        </div>
-                        </ProductWrapper>
-                    )
+                      </ProductWrapper>
+                    );
           }
 }
 
